0eaf043af1ba1d6d22f01ffa8ece3da80c264eb1
jimafisk <jimafisk@gmail.com>
2020-04-07 19:02:33 -0400

    Add sub component to template.

--- layout/content/Index.svelte
+++ layout/content/Index.svelte
@@ -1,11 +1,13 @@
 <script>
 	export let name;
 	export let allNodes;
+  import Component from './Component.svelte';
 </script>
 
 <section id="intro">
 	<img alt="plenti logo" src="/build/plenti.svg" />
 	<h1>{name}</h1>
+  <Component />
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<h3>Recent blog posts:</h3>
 </section>
