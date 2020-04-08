6efae3863b1c45c2b9937323a4ef2e9dd9291ad4
jimafisk <jimafisk@gmail.com>
2020-04-08 15:17:30 -0400

    Pass actual node information for creating html.

--- layout/ejected/build.js
+++ layout/ejected/build.js
@@ -7,13 +7,20 @@ const io = [];
 
 nodes.forEach(node => {
   let filename = node.filename.substr(0, node.filename.lastIndexOf(".")) + ".html";
-  io.push({src: 'Html.svelte', dest: filename});
+  let route = node.type.charAt(0).toUpperCase() + node.type.slice(1); 
+  io.push({src: 'Html.svelte', dest: filename, route: route, node: node, nodes: nodes});
 });
 
 let ioFull = io.map(x => ({
   src: path.join(path.resolve(), 'layout/global/', x.src),
   dest: path.join(path.resolve(), 'public/testout', x.dest),
+  route: x.route,
+  node: x.node,
+  nodes: x.nodes,
 }));
+
+//let test = ioFull.map(({src}) => src);
+//console.log(test);
 
 const main = () =>
   fs.promises
@@ -22,7 +29,8 @@ const main = () =>
 
     .then(() =>
       Promise.all(
-        makeHtmlWithStyle(ioFull.map(({src}) => src), 'Nav', {filename:'test'}, nodes).map((html, i) =>
+        //makeHtmlWithStyle(ioFull.map(({src}) => src), 'Pages', {filename:'test'}, nodes).map((html, i) =>
+        makeHtmlWithStyle(ioFull).map((html, i) =>
           fs.promises.writeFile(ioFull[i].dest, html),
         ),
       ),
--- layout/ejected/makeHtml.js
+++ layout/ejected/makeHtml.js
@@ -6,9 +6,9 @@ import 'svelte/register.js';
 const renderHtml = (input, Route, node, allNodes) => {
   /* eslint-disable-next-line global-require */
   const Comp = relative(input, process.cwd()).default;
-  //console.log('route is: ' + Route);
-  //console.log('node is: ' + JSON.stringify(node));
-  //console.log('All nodes are: ' + JSON.stringify(allNodes));
+  console.log('route is: ' + Route);
+  console.log('node is: ' + JSON.stringify(node));
+  console.log('All nodes are: ' + JSON.stringify(allNodes));
   let props = {};
   props.Route = Route;
   props.node = node;
@@ -16,9 +16,12 @@ const renderHtml = (input, Route, node, allNodes) => {
   return Comp.render(props);
 };
 
-const makeHtmlWithStyle = (inputs, Route, node, allNodes) =>
+//const makeHtmlWithStyle = (inputs, Route, node, allNodes) =>
+const makeHtmlWithStyle = inputs =>
+  //inputs.forEach(input 
   inputs
-    .map(function(input) { return renderHtml(input, Route, node, allNodes) })
+    //.map(input => renderHtml(input, Route, node, allNodes))
+    .map(input => renderHtml(input.src, input.route, input.node, input.nodes))
     .map(rendered =>
       injectIntoHead(`\n<style>${rendered.css.code}</style>\n`)(rendered.html),
     );
