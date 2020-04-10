9c0498ed448faa58e6f60d79c89cb32a4a695215
jimafisk <jimafisk@gmail.com>
2020-04-10 11:02:39 -0400

    Move makeTitle into separate script.

--- layout/global/Html.svelte
+++ layout/global/Html.svelte
@@ -1,23 +1,9 @@
 <script>
   import Head from './Head.svelte';
   import Nav from './Nav.svelte';
-  import Pages from '../content/Pages.svelte';
+  import { makeTitle } from './Scripts.svelte';
 
   export let Route, node, allNodes;
-
-  const makeTitle = filename => {
-  if (filename == '_index.json') {
-    return 'Home';
-  } else if (filename) {
-    // Remove file extension.
-    filename = filename.split('.').slice(0, -1).join('.');
-    // Convert underscores and hyphens to spaces.
-    filename = filename.replace(/_|-/g,' ');
-    // Capitalize first letter of each word.
-    filename = filename.split(' ').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
-  } 
-  return filename;
-  }
 </script>
 
 <html lang="en">
@@ -25,9 +11,13 @@
 <body>
   <Nav />
   <main>
-    <h1>{node.filename}{Route}</h1>
-    <!--
+    <h1>{node.filename}</h1>
+    <details>
+      <summary>Click to show Route class</summary>
+      <pre>{Route}</pre>
+    </details>
     <svelte:component this={Route} {...node.fields} {allNodes} />
+    <!--
     -->
   </main>
 </body>
