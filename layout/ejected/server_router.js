27cf242d0d67583ee03964f76b0b6aa8e8ef755f
jimafisk <jimafisk@gmail.com>
2020-04-13 11:23:06 -0400

    Add inject back in.

--- layout/ejected/server_router.js
+++ layout/ejected/server_router.js
@@ -4,6 +4,16 @@ import nodes from './nodes.js';
 import 'svelte/register.js';
 import relative from 'require-relative';
 
+const injectString = (order, content, element, html) => {
+  if (html) {
+		if (order == 'prepend') {
+			html.replace(element, content + element);
+		} else if (order == 'append') {
+			html.replace(element, element + content);
+		}
+  }
+};
+
 nodes.forEach(node => {
   let sourceFilename = node.type + '.svelte';
   let sourcePath = path.join(path.resolve(), 'layout/content/' + sourceFilename);
@@ -19,7 +29,20 @@ nodes.forEach(node => {
   };
   // Create HTML file.
   const component = relative(topLevelComponent, process.cwd()).default;
-  const { html, css } = component.render(props);
+  let { html, css } = component.render(props);
+  // Inject Style.
+  let style = `<style>${css.code}</style>`;
+  html = injectString('prepend', style, '</head>', html);
+  // Inject SPA entry point.
+  let entryPoint = `
+  <script type="module" src="https://unpkg.com/dimport?module" data-main="/build/spa/main.js"></script>
+  <script nomodule src="https://unpkg.com/dimport/nomodule" data-main="/build/spa/main.js"></script>
+	`;
+  html = injectString('prepend', entryPoint, '</head>', html);
+  // Inject ID used to hydrate SPA.
+  let hydrator = ' id="hydrate-plenti"';
+  html = injectString('append', hydrator, '<html', html);
+  // Write HTML files to filesystem.
   fs.promises.mkdir(path.join(path.resolve(), 'public'), {recursive: true})
   fs.promises.writeFile(destPath, html);
 });
